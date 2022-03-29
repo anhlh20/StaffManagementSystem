284afd34591eb5c5563901d6ce9b6b8fd4ba7279
Lã Hồng Anh <admin@example.com>
2022-03-29 17:57:33 +0700

    add EmployeeManagement

---
+++ src/business/EmployeeManagement.java
@@ -0,0 +1,16 @@
+package business;
+
+import entity.Employee;
+
+import java.util.ArrayList;
+
+public class EmployeeManagement {
+    private ArrayList<Employee> empList;
+
+    public ArrayList<Employee> getEmployeeList(){
+        return this.empList;
+    }
+
+
+
+}
