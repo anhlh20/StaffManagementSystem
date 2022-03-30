37043792b943c328aa4b1286bd73a61d2e5c45ef
Lã Hồng Anh <admin@example.com>
2022-03-30 11:59:22 +0700

    update searchByName

--- src/business/EmployeeManagement.java
+++ src/business/EmployeeManagement.java
@@ -3,12 +3,36 @@ package business;
 import entity.Employee;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class EmployeeManagement {
     private ArrayList<Employee> empList;
 
+    public EmployeeManagement(){
+        empList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee e){
+        empList.add(e);
+    }
+
     public ArrayList<Employee> getEmployeeList(){
         return this.empList;
+    }
+    public ArrayList<Employee> searchByName(String name){
+        ArrayList<Employee> foundByName = new ArrayList<>();
+        ArrayList<Employee> empListOrigin = this.getEmployeeList();
+
+        if(name.equals("") || name.trim() == ""){
+            return foundByName;
+        }
+        for (Employee e : empListOrigin){
+            String s = e.getName().toUpperCase();
+            if(s.contains(name.toUpperCase())){
+                foundByName.add(e);
+            }
+        }
+        return foundByName;
     }
 
 
